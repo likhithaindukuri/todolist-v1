188b358224bbddde6fedbc8532a5fd75a386e063
Indukuri Likhitha <likhithaindukuri07@gmail.com>
2023-04-02 22:41:18 +0530

    Modified

--- app.js
+++ app.js
@@ -8,19 +8,22 @@ const app = express();
 
 app.get("/",function(req,res){
   var today = new Date();
+  var currentDay = today.getDay();
 
-  if(today.getDay()==4)
+  if(currentDay==6 || currentDay==0)
   {
-    res.send("Yay it's the weekend!");
+    res.write("<h1>Yay it's the weekend!</h1>");
   }
   else
   {
-    res.send("Boo! I have to work!");
+    res.write("<p>It is not the weekend!</p>");
+    res.write("<h1>Boo! I have to work!</h1>");
+    res.send();
   }
-  
+
 });
 
 
-app.Listen(3000,function(){
+app.listen(3000,function(){
   console.log("Server started on port 3000");
 });
